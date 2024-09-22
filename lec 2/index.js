c5e79e1c584dff968254157420912a4b4f78c492
RazaMuhammad5758 <waistband161.com>
2024-09-22 20:37:28 +0500

    lec 2 day 2

--- lec 2/index.js
+++ lec 2/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -10,6 +12,10 @@ app.get("/facebook", (req, res) => {
     res.send("Raza Muhammad")
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.get("/youtube", (req,res)=>{
+    res.send("<h2>Welcome to youtube</h2>")
+})
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening on port ${process.env.PORT || port}`)
 })
